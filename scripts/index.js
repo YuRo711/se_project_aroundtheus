98401a51db0ba80ce85846a48edd83fdb23155ce
YuRo <92213643+YuRo711@users.noreply.github.com>
2023-11-11 20:25:21 +0200

    info update + cards

--- scripts/index.js
+++ scripts/index.js
@@ -27,16 +27,47 @@ let initialCards = [
 
 let editButton = document.querySelector(".profile__edit-button");
 let closeButton = document.querySelector(".modal__close-button");
+let saveButton = document.querySelector(".modal__save-button");
 editButton.addEventListener("click", openModal);
 closeButton.addEventListener("click", closeModal);
+saveButton.addEventListener("click", updateInfo);
+
+let modal = document.querySelector(".modal");
+let inputFields = modal.querySelectorAll(".modal__input");
+let nameInput = inputFields[0];
+let descInput = inputFields[1];
+let nameText = document.querySelector(".profile__name");
+let descText = document.querySelector(".profile__description");
+
+let cards = document.querySelector(".cards");
+for(data of initialCards) {
+    let card = getCardElement(data);
+    cards.appendChild(card);
+}
+
 
 
 function openModal() {
-    let modal = document.querySelector(".modal");
     modal.classList.add("modal_opened");
 }
 
 function closeModal() {
-    let modal = document.querySelector(".modal");
     modal.classList.remove("modal_opened");
+}
+
+function updateInfo(event) {
+    event.preventDefault();
+    nameText.textContent = nameInput.value;
+    descText.textContent = descInput.value;
+    closeModal();
+}
+
+function getCardElement(data) {
+    let name = data.name;
+    let image = data.link;
+    let template = document.getElementById("card");
+    let card = template.content.cloneNode(true);
+    card.querySelector(".card__image").setAttribute("src", image);
+    card.querySelector(".card__title").textContent = name;
+    return card;
 }
